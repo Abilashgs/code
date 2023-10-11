5314138d73f39bed7b4654dd6889ff9efd0c8a0a
Abilash GS <p2755@ramco.com>
2023-10-11 12:22:42 +0530

    d

---
+++ JAVA/Helloworld.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int y;
+        y=20;
+        for(int i=0;i<=y;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
